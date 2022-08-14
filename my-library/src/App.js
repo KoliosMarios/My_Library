892120069d59490d92b19fe128f31cd3e7d90878
KoliosMarios <koliosmarios@gmail.com>
2022-08-14 17:48:53 +0300

    adding comments

--- my-library/src/App.js
+++ my-library/src/App.js
@@ -12,19 +12,24 @@ import { db } from "./firebase-config";
 import { async } from "@firebase/util";
 
 function App() {
+  //make a reference to the books collection
   const colRef = collection(db, "books");
 
+  //getting the inputs so we can empty them after submitting the new book
   const titleInput = document.getElementById("title");
   const authorInput = document.getElementById("author");
   const pagesInput = document.getElementById("pages");
   const readInput = document.getElementById("read");
 
+  // setting the state for the books array
   const [books, setBooks] = useState([]);
+  //setting the states for the details of every book
   const [newTitle, setNewTitle] = useState("");
   const [newAuthor, setNewAuthor] = useState("");
   const [newPages, setNewPages] = useState(0);
   const [newRead, setNewRead] = useState(false);
 
+  //function for getting and renewing the books array
   const getBooks = async () => {
     const data = await getDocs(colRef);
     setBooks(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
@@ -35,6 +40,7 @@ function App() {
   }, []);
 
   const createBook = async () => {
+    //getting the data from the input and informing the states
     await addDoc(colRef, {
       title: newTitle,
       author: newAuthor,
@@ -42,24 +48,31 @@ function App() {
       read: Boolean(newRead),
     });
     getBooks();
+
+    //empty the states after we add the new book and setting read property to false
     setNewTitle("");
     setNewAuthor("");
     setNewPages("");
+    setNewRead(false);
+
+    //empty the inputs and unchecking the checkbox
     titleInput.value = " ";
     authorInput.value = " ";
     pagesInput.value = " ";
     readInput.checked = false;
-    setNewRead(false);
   };
 
+  //function to delete a book by using the deleteDoc from firebase
   const deleteBook = async (id) => {
     const book = doc(db, "books", id);
     await deleteDoc(book);
     getBooks();
   };
 
+  //function to update the read status of a book by using the updateDoc from firebase
   const updateBook = async (id, read) => {
     const book = doc(db, "books", id);
+    //we check if the read property is true or false and then change it to the opposite
     const newFields = { read: !read };
     await updateDoc(book, newFields);
     getBooks();
@@ -115,7 +128,9 @@ function App() {
           }}
         ></input>
         <br />
-        <button id="create_book" onClick={createBook}>Create Book</button>
+        <button id="create_book" onClick={createBook}>
+          Create Book
+        </button>
       </div>
 
       <table>
@@ -130,14 +145,16 @@ function App() {
         <tbody>
           {books.map((book) => {
             return (
+              // we present all the books in the array in a table
               <tr key={book.id}>
                 <td>{book.title}</td>
                 <td>{book.author}</td>
                 <td>{book.pages}</td>
+                {/* if the read property is true we write yes and if it's false we write no */}
                 <td>{book.read ? "Yes" : "No"}</td>
                 <td>
                   <button
-                  id="delete_button"
+                    id="delete_button"
                     onClick={() => {
                       deleteBook(book.id);
                     }}
@@ -147,7 +164,7 @@ function App() {
                 </td>
                 <td>
                   <button
-                  id="change_button"
+                    id="change_button"
                     onClick={() => {
                       updateBook(book.id, book.read);
                     }}
